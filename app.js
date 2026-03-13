70c1778a3bfd5c8a4ccc1e60f31470ddff1a3123
Gawain26 <fostergawain92@gmail.com>
2026-03-13 01:15:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ function searchRecipes() {
   const term = document.getElementById("searchBox").value
   console.log("Searching for:", term)
 }
+
+let recipes = []
+let items = []
+
+async function loadData() {
+
+    const recipeRes = await fetch("data/recipes.json")
+    recipes = await recipeRes.json()
+
+    const itemRes = await fetch("data/items.json")
+    items = await itemRes.json()
+
+    console.log("Recipes:", recipes.length)
+    console.log("Items:", items.length)
+}
+
+loadData()
